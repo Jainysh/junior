8a4936c63ec2c85cdf609e8af49d9e7ff93440e5
Yash Jain <yjain56@yahoo.com>
2023-04-15 14:10:30 +0530

    UX change, added countdown contianer

---
+++ components/Countdown/countdown.tsx
@@ -0,0 +1,74 @@
+// components/CountdownTimer.js
+
+import React, { useEffect, useState } from "react";
+
+type CountdownTimerProps = {
+  message: string;
+  dueDate: Date;
+};
+const CountdownTimer = ({ dueDate, message }: CountdownTimerProps) => {
+  const [months, setMonths] = useState(0);
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date().getTime();
+      const timeRemaining = dueDate.getTime() - now;
+
+      const months = Math.floor(timeRemaining / (1000 * 60 * 60 * 24 * 30));
+      const days = Math.floor(
+        (timeRemaining % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24)
+      );
+      const hours = Math.floor(
+        (timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor(
+        (timeRemaining % (1000 * 60 * 60)) / (1000 * 60)
+      );
+      const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+      setMonths(months);
+      setDays(days);
+      setHours(hours);
+      setMinutes(minutes);
+      setSeconds(seconds);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [dueDate]);
+
+  return (
+    <div className="text-center bg-black rounded-lg p-2 shadow m-2">
+      <h2 className="text-2xl font-bold mb-3">{message}</h2>
+      <div className="flex justify-center items-center flex-wrap">
+        <div className="bg-gray-200 text-gray-800 p-4 rounded-lg mx-2 mb-4">
+          <div className="text-4xl font-semibold">{months}</div>
+          <div className="text-sm">Months</div>
+        </div>
+        <div className="bg-gray-200 text-gray-800 p-4 rounded-lg mx-2 mb-4">
+          <div className="text-4xl font-semibold">{days}</div>
+          <div className="text-sm">Days</div>
+        </div>
+        <div className="bg-gray-200 text-gray-800 p-4 rounded-lg mx-2 mb-4">
+          <div className="text-4xl font-semibold">{hours}</div>
+          <div className="text-sm">Hours</div>
+        </div>
+        <div className="bg-gray-200 text-gray-800 p-4 rounded-lg mx-2 mb-4">
+          <div className="text-4xl font-semibold">{minutes}</div>
+          <div className="text-sm">Minutes</div>
+        </div>
+        <div className="bg-gray-200 text-gray-800 p-4 rounded-lg mx-2 mb-4">
+          <div className="text-4xl font-semibold">{seconds}</div>
+          <div className="text-sm">Seconds</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CountdownTimer;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,85 +1,32 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import { useEffect, useState } from "react";
+// pages/index.js
 
-const inter = Inter({ subsets: ["latin"] });
+import React from "react";
+import styles from "../styles/Home.module.css";
+import CountdownTimer from "@/components/Countdown/countdown";
 
-type DateTime = {
-  seconds: number;
-  minutes: number;
-  hours: number;
-  days: number;
-  months: number;
-};
-export default function Home() {
-  const [daysLeft, setDaysLeft] = useState<DateTime>();
-
-  // bug in time calculation
-  const diff_hours = () => {
-    const today = new Date();
-    const deliveryDate = new Date("July 22, 2023 00:00:01");
-    var diff = (deliveryDate.getTime() - today.getTime()) / 1000;
-
-
-    const seconds = Math.abs(Math.round(diff));
-    const minutes = Math.abs(Math.round(seconds / 60));
-    const hours = Math.abs(Math.round(minutes / (60)));
-    const days = Math.abs(Math.round(hours / 24));
-    const months = Math.abs(
-      Math.round(deliveryDate.getMonth() - today.getMonth())
-    );
-    setDaysLeft({
-      seconds: seconds%(60),
-      minutes: minutes%(60),
-      hours: hours%(24),
-      days: days%(30),
-      months,
-    });
-    console.log(seconds,minutes)
-  };
-
-  useEffect(() => {
-    const interval = setInterval(() => diff_hours(), 1000);
-    return () => clearInterval(interval)
-  }, []);
+const HomePage = () => {
+  // Define your due date here
+  const dueDate = new Date("July 22, 2023 00:00:01");
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-center align-center font-mono text-sm lg:flex">
-        <div className="top-0  justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          {/* <div> */}
-          Join us in welcoming our junior champ🤞🏻
+    <>
+      <div className={styles.gradientBg}>
+        <div className={styles.timerContainer}>
+          <CountdownTimer dueDate={dueDate} message="Awwwating!" />
         </div>
-        {/* <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none"> */}
-        {/* <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a> */}
-        {/* </div> */}
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <div style={{ fontSize: "20px", fontWeight: "bold" }}>
-          {daysLeft?.months} months | {daysLeft?.days} days | {daysLeft?.hours}{" "}
-          hours | {daysLeft?.minutes} min | {daysLeft?.seconds} secs
+        <div className={styles.container}>
+          <div className={styles.greetingContainer}>
+            <h1 className={styles.greetingText}>
+              Welcome to Our Junior&apos;s Website
+            </h1>
+            <p className={styles.subText}>
+              We are excitedly awaiting our little bundle of joy!
+            </p>
+          </div>
         </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        {/* {timeLeft} */}
-      </div>
-    </main>
+    </>
   );
-}
+};
+
+export default HomePage;
