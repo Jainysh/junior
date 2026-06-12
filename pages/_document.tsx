938e1b0fc7c223d61668922c7beb3de1430acf44
Yash Jain <yjain56@yahoo.com>
2026-06-12 11:17:04 +0530

    feat: remove ms clarity integration

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,17 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(
-                function(c,l,a,r,i,t,y){
-                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-                })(window, document, "clarity", "script", "gqrlgsq59v"
-							);`,
-          }}
-        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
